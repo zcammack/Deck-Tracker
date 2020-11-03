419fcbe2d31e075b2707a3accd919c96c167773e
zcammack <zack.cammack@gmail.com>
2020-11-03 13:50:45 -0600

    Fixed a few minor errors in the card controller. Added a before_action line to utilize the set_card! method. Finished out update and destroy actions.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
-
+        before action :set_card!, only: [:show, :edit, :update, :destroy]
     def new
         @card = Card.new
     end
@@ -26,12 +26,19 @@ class CardsController < ApplicationController
     end
 
     def update
-
+        if @card.update(card_params)
+            redirect_to card_path(@card)
+        else
+            render "edit"
+        end
     end
 
     def destroy
-
+        @card.destroy
+        redirect_to cards_path
     end
+
+    private
 
     def set_card!
         @card = Card.find(params[:id])
