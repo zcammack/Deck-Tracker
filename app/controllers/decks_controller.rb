e32bf2581b9d69af146d74e783751a68fb6d7d0f
zcammack <zack.cammack@gmail.com>
2020-11-03 13:56:53 -0600

    Created User and Deck controllers by copy/pasting card controller and editing the values. This will be edited further as needed... but essentially these controllers should function the same. Need to look at params after building some views.

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,50 @@
+class DecksController < ApplicationController
+        before action :set_decks!, only: [:show, :edit, :update, :destroy]
+    def new
+        @deck = Deck.new
+    end
+
+    def create
+        @deck = Deck.new(deck_params)
+        if @deck.save
+            redirect_to deck_path(@deck)
+        else
+            render "new"
+        end
+    end
+
+    def index
+        @decks = Deck.all
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @deck.update(deck_params)
+            redirect_to deck_path(@deck)
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @deck.destroy
+        redirect_to decks_path
+    end
+
+    private
+
+    def set_decks!
+        @deck = Deck.find(params[:id])
+    end
+
+    def deck_params
+        params.require(:deck).permit()
+    end
+end
