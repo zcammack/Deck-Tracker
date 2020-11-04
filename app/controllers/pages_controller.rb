852bcecdd1c41b8cd62e618cedbc7f43120b3a06
zcammack <zack.cammack@gmail.com>
2020-11-04 15:59:52 -0600

    Expanded pages controller. Made multiple new partials pages that should allow the program to display a large number of card art on the page, making for an easy experience for the user to read and select cards.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
+    session[:images] = session[:images] || []
+
+    if session[:images].empty? || params["button_action"] == "refresh"
+      session[:images] = get_scryfall_images
+    end
   end
 end
